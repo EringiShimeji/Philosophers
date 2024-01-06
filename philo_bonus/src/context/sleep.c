f828da921624733e125c6092dcdc9960b1bc00e3
EringiShimeji <eringi.shimeji.5296@gmail.com>
2024-01-06 11:37:34 +0900

    fix norm

--- philo_bonus/src/context/sleep.c
+++ philo_bonus/src/context/sleep.c
@@ -6,7 +6,7 @@
 /*   By: smatsuo <smatsuo@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 12:35:50 by smatsuo           #+#    #+#             */
-/*   Updated: 2023/12/27 14:18:32 by smatsuo          ###   ########.fr       */
+/*   Updated: 2024/01/06 11:31:16 by smatsuo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	psleep(t_philo *philo)
 	if (philo->num_of_eaten_meals == philo->ctx->must_eat)
 	{
 		set_has_finished_meal(philo, true);
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
